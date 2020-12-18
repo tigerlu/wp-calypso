f957fe70882d247995e55e79f232d1ad77077d62
nbloomf <nathan.bloomfield@automattic.com>
2020-12-18 09:49:34 -0600

    purchases/paths

--- client/my-sites/purchases/paths.ts
+++ client/my-sites/purchases/paths.ts
@@ -1,53 +1,44 @@
-/**
- * Internal Dependencies
- */
-import { isEnabled } from 'calypso/config';
-
 export const getManagePurchaseUrlFor = (
 	targetSiteSlug: string,
 	targetPurchaseId: string | number
-) => `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchaseId }`;
+): string => `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchaseId }`;
 
 export const getConfirmCancelDomainUrlFor = (
 	targetSiteSlug: string,
 	targetPurchaseId: string | number
-) => `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchaseId }/confirm-cancel-domain`;
+): string =>
+	`/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchaseId }/confirm-cancel-domain`;
 
 export const getCancelPurchaseUrlFor = (
 	targetSiteSlug: string,
 	targetPurchaseId: string | number
-) => `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchaseId }/cancel`;
+): string => `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchaseId }/cancel`;
 
-export const getPurchaseListUrlFor = ( targetSiteSlug: string ) =>
+export const getPurchaseListUrlFor = ( targetSiteSlug: string ): string =>
 	`/purchases/subscriptions/${ targetSiteSlug }`;
 
 export const getAddPaymentMethodUrlFor = (
 	targetSiteSlug: string,
 	targetPurchase: { id: string | number }
-) =>
-	isEnabled( 'purchases/new-payment-methods' )
-		? `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchase }/payment-method/add`
-		: `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchase }/payment/add`;
+): string => `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchase }/payment-method/add`;
 
-export const getAddNewPaymentMethod = ( targetSiteSlug: string ) =>
-	isEnabled( 'purchases/new-payment-methods' )
-		? `/purchases/add-payment-method/${ targetSiteSlug }`
-		: `/purchases/add-credit-card/${ targetSiteSlug }`;
+export const getAddNewPaymentMethod = ( targetSiteSlug: string ): string =>
+	`/purchases/add-payment-method/${ targetSiteSlug }`;
 
-export const getPaymentMethodsUrlFor = ( targetSiteSlug: string ) =>
+export const getPaymentMethodsUrlFor = ( targetSiteSlug: string ): string =>
 	`/purchases/payment-methods/${ targetSiteSlug }`;
 
 export const getChangePaymentMethodUrlFor = (
 	targetSiteSlug: string,
 	targetPurchase: { id: string | number },
 	targetCardId: { id: string | number }
-) =>
-	isEnabled( 'purchases/new-payment-methods' )
-		? `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchase }/payment-method/change/${ targetCardId }`
-		: `/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchase }/payment/edit/${ targetCardId }`;
+): string =>
+	`/purchases/subscriptions/${ targetSiteSlug }/${ targetPurchase }/payment-method/change/${ targetCardId }`;
 
-export const getReceiptUrlFor = ( targetSiteSlug: string, targetReceiptId: string | number ) =>
-	`/purchases/billing-history/${ targetSiteSlug }/${ targetReceiptId }`;
+export const getReceiptUrlFor = (
+	targetSiteSlug: string,
+	targetReceiptId: string | number
+): string => `/purchases/billing-history/${ targetSiteSlug }/${ targetReceiptId }`;
 
-export const getBillingHistoryUrlFor = ( targetSiteSlug: string ) =>
+export const getBillingHistoryUrlFor = ( targetSiteSlug: string ): string =>
 	`/purchases/billing-history/${ targetSiteSlug }`;
